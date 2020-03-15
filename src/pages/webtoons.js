b0ca447bfbc71a39d04cd00fe69c60ec66408759
g-gyong <kotn7566@gmail.com>
2020-03-15 15:00:04 +0900

    웹툰 올리기

--- src/pages/webtoons.js
+++ src/pages/webtoons.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import { Container } from '../design-system/container'
+import Webtoons from '../components/webtoons'
 
+//데이터를 가지고 와서 셋팅
 function WebtoonsPage() {
-   return <Container>Webtoons</Container>
+  return (
+    <Container>
+      <Webtoons />
+    </Container>
+  )
 }
 
 export default WebtoonsPage
